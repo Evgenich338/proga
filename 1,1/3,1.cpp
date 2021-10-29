0d4c4c288ed747abff3c113c367e46ea4586e0c5
Evgenich338 <mriedchienko@mail.ru>
2021-10-29 13:41:15 +0300

    Третье задание

---
+++ 1,1/3,1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+/
+*\brief Функция для расчета y
+* \param x Параметр x
+* \return Параметр y
+* /
+double GetY(double x);
+
+/
+*\brief Ввод значения х в программу
+* \return Конечное значение
+* /
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	double x;
+	cout « "Введите значение x = ";
+	cin » x;
+	const int LOW_BOUND = 1;
+	const int UP_BOUND = 3;
+	const double step = 0.2;
+
+
+	for (x; x >= LOW_BOUND && x <= UP_BOUND; x += step)
+
+	{
+		if (x >= LOW_BOUND && x <= UP_BOUND)
+		{
+			auto y = GetY(x);
+
+			cout « " y = " « y « "\n";
+		}
+		else
+		{
+			auto y = GetY(x);
+			cout « " y = " « y « "\n" « "Нет решения, так как х не входит в область определения " « endl;
+		}
+	}
+	return 0;
+}
+
+double GetY(double x)
+{
+	return sin(log(x)) - cos(log(x)) + 2 * log(x);
+}
