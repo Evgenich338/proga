833339eb6640083bbc792a9654838ff2e05a695e
Evgenich338 <mriedchienko@mail.ru>
2021-11-26 00:06:32 +0300

    Задание 2.1 переделано

--- 2.1.cpp
+++ 2.1.cpp
@@ -6,41 +6,41 @@ using namespace std;
 *\ param value1 переменная вводимая пользователем
 *\ param value2 переменная вводимая пользователем
 */
-void sum(double value1, double value2);
+double addition(double value1, double value2);
 
 /**
 * \brief вычитание
 *\ param value1 переменная вводимая пользователем
 *\ param value2 переменная вводимая пользователем
 */
-void difff(double value1, double value2);
+double difff(double value1, double value2);
 
 /**
 * \brief умножение
 *\ param value1 переменная вводимая пользователем
 *\ param value2 переменная вводимая пользователем
 */
-void proizv(double value1, double value2);
+double multiplication(double value1, double value2);
 
 /**
 * \brief деление
 *\ param value1 переменная вводимая пользователем
 *\ param value2 переменная вводимая пользователем
 */
-void chass(double value1, double value2);
+double chass(double value1, double value2);
 
 enum path
 {
-Sum = 1,
+sum = 1,
 diff = 2,
-proiz = 3,
+multiply = 3,
 chas = 4
 };
 
 /**
 * \brief Точка входа в программу.
-* \param value1 - переменная 1
-* \param value2 - переменная 2
+* \param value1 - переменная вводимая user-ом
+* \param value2 - переменная вводимая user-ом
 * \param choice - переменная для выбора алгоритма
 * \return Код ошибки (0 - успех).
 */
@@ -48,34 +48,38 @@ int main()
 {
 setlocale(LC_ALL, "Russian");
 
-double value1, value2
+double valueA, valueB;
 int choice;
 cout << "Введите а - ";
-cin >> value1;
+cin >> valueA;
 cout << "Введите b - ";
-cin >> value2;
+cin >> valueB;
 cout << "выберите что вы хотите сделать с переменными \n 1.сложение\n 2. вычитание\n 3.умножение\n 4.деление\n";
 cin >> choice;
 const auto filling = static_cast<path>(choice);
 switch (filling) {
-case path::Sum:
+case path::sum:
 {
-sum( value1, value2);
+double rezult = addition( valueA, valueB);
+cout<<rezult;
 break;
 }
 case path::diff:
 {
-difff(value1,value2);
+double rezult = difff(valueA,valueB);
+cout<<rezult;
 break;
 }
-case path::proiz:
+case path::multiply:
 {
-proizv(value1, value2);
+double rezult = multiplication(valueA, valueB);
+cout<<rezult;
 break;
 }
 case path::chas:
 {
-chass(value1, value2);
+double rezult = chass(valueA, valueB);
+cout<<rezult;
 break;
 }
 default:
@@ -84,20 +88,20 @@ break;
 }
 }
 
-void sum(double value1, double value2)
+double addition(double value1, double value2)
 {
-cout<<value1+value2;
+return value1+value2;
 }
 
-void difff(double value1, double value2)
+double difff(double value1, double value2)
 {
-cout<<value1-value2;
+return value1-value2;
 }
 
-void proizv(double value1, double value2){
-cout<<value1*value2;
+double multiplication(double value1, double value2){
+return value1*value2;
 }
 
-void chass(double value1, double value2){
-cout<<value1/value2;
+double chass(double value1, double value2){
+return value1/value2;
 }
